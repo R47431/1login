24a8323249692bea1bc589423a3f38ae82c1d9a5
rafael <joaorafael1223@hotmail.com>
2023-07-08 15:15:33 -0300

    usando storage

---
+++ frontend/src/app/service/storage.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StorageService {
+  private stotage: Storage;
+
+  constructor() {
+    this.stotage = window.localStorage;
+  }
+
+  set(key: string, value: any): boolean {
+    if (this.stotage) {
+      this.stotage.setItem(key, JSON.stringify(value));
+    }
+    return false;
+  }
+
+  get(key: string): any {
+    if (this.stotage) {
+      return JSON.parse(this.stotage.getItem(key) || 'null');
+    }
+    return null;
+  }
+
+  remove(key: string): boolean {
+    if (this.stotage) {
+      this.stotage.removeItem(key);
+    }
+    return false;
+  }
+  clear(key: string): boolean {
+    if (this.stotage) {
+      this.stotage.clear();
+    }
+    return false;
+  }
+}
